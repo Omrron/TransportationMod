5c57e5353f2d117b31195635584a4f9d47e24404
danielyamayam <inbar.sofer@gmail.com>
2024-11-02 15:30:16 +0200

    ADD ITEM

---
+++ src/main/java/com/transportation/TransportationMod/item/moditem.java
@@ -0,0 +1,20 @@
+package com.transportation.TransportationMod.item;
+
+import com.transportation.TransportationMod.TransportationMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class moditem {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TransportationMod.MODID);
+
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("corrupted bow",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
